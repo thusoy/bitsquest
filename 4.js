e6b1d407d554fdfd62204275c661f4beffe43797
Tarjei Husøy <tarjei@comoyo.com>
2013-07-08 03:17:03 +0200

    Solved #4.

---
+++ 4.js
@@ -0,0 +1,37 @@
+/*
+ * Some times it helps to retain some state, state like "did I just come from 
+ * back there?" You can keep any data you wish to preserve across events 
+ * in the closure created by the initial execution of your code.
+ *
+ * For example:
+ *
+ *   var beenThere = false;
+ *   
+ *   this.on('sensor:left', function() {
+ *      beenThere = true;
+ *   });
+ *
+ *   this.on('sensor:right', function() {
+ *      if (beenThere) {
+ *         // already been left
+ *      }
+ *   });
+ */
+
+ /**
+ * Tactic: Move up and to the right until we hit the roof, move until we find the 
+ * right wall, move down and out.
+ */
+
+this.on("start", function(){
+  this.thrusters.left(true);
+  this.thrusters.bottom(true);
+});
+
+this.on("sensor:top", function(contact){
+  this.thrusters.bottom(false);
+});
+
+this.on("sensor:right", function(contact){
+  this.thrusters.top(true);
+});
